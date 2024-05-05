8359498fbe9018940f57e9dfca10d25ded9972dd
Juan Forez <juanks339@gmail.com>
2024-05-05 17:13:15 -0500

    NEW: Save locatiosn in cards

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+  
+  @ViewChild('map') divMap?: ElementRef;
+  @Input() lngLat?: [number, number];
 
+  ngAfterViewInit(): void {
+    this.renderMap();
+  }
+
+  public renderMap() {
+    if( !this.divMap?.nativeElement) throw 'Map Not Found'
+    if ( !this.lngLat ) throw "LngLat can't null";
+
+    if (this.divMap) {
+      const map = new Map({
+        container: this.divMap.nativeElement,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: this.lngLat,
+        interactive: false,
+        zoom: 15
+      });
+
+      new Marker().setLngLat(this.lngLat).addTo(map)
+    }
+  }
 }
