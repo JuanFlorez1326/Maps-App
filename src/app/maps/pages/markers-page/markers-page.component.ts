943243a0cbab5a61c1a75a5223467ce8a7898880
Juan Forez <juanks339@gmail.com>
2024-05-05 16:26:02 -0500

    NEW: Add marker to map

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
+
+interface MarkerAndColor {
+  color: string,
+  marker: Marker
+}
 
 @Component({
   selector: 'app-markers-page',
@@ -7,4 +13,47 @@ import { Component } from '@angular/core';
 })
 export class MarkersPageComponent {
 
+  public currentLngLat: LngLat = new LngLat(-75.7, 4.5);
+  public allMarkers: MarkerAndColor[] = [];
+  public zoomLevel: number = 13;
+  public map?: Map;
+
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.renderMap();
+  }
+    
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+
+  public renderMap() {
+    if (this.divMap) {
+      this.map = new Map({
+        container: this.divMap.nativeElement,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: this.currentLngLat,
+        zoom: this.zoomLevel
+      });
+    }
+  }
+
+  public createMarker() {
+    if(!this.map) return;
+    const color = '#xxxxxx'.replace(/x/g, y => (Math.random()*16|0).toString(16));
+    const lngLat = this.map.getCenter();
+    this.addMarker( lngLat, color);
+  }
+
+  public addMarker( lngLat: LngLat, color: string) {
+    if(!this.map) return;
+    const marker = new Marker({ color, draggable: true }).setLngLat(lngLat).addTo(this.map);
+    this.allMarkers.push({ marker, color });
+  }
+
+  public deleteMarker( index: number ) {
+    this.allMarkers[index].marker.remove();
+    this.allMarkers.splice(index, 1);
+  }
 }
